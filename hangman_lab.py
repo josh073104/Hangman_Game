e547a6ba5762085bb963a1c087101a859f20eb9f
josh073104 <140757636+josh073104@users.noreply.github.com>
2024-02-15 16:51:58 -0600

    Add files via upload

---
+++ hangman_lab.py
@@ -0,0 +1,174 @@
+
+
+# import libraries
+import random
+
+pic1 = """            +---+
+            |   |
+                |
+                |
+                |
+                |
+          ========="""
+pic2 = """            +---+
+            |   |
+            O   |
+                |
+                |
+                |
+          ========="""
+pic3 = """            +---+
+            |   |
+            O   |
+            |   |
+                |
+                |
+          ========="""
+pic4 = """            +---+
+            |   |
+            O   |
+           /|   |
+                |
+                |
+          ========="""
+pic5 = """            +---+
+            |   |
+            O   |
+           /|\  |
+                |
+                |
+          ========="""
+pic6 = """            +---+
+            |   |
+            O   |
+           /|\  |
+           /    |
+                |
+          ========="""
+pic7 = """            +---+
+            |   |
+            O   |
+           /|\  |
+           / \  |
+                |
+          ========="""
+pic8 = """            +--------+
+            |        |
+       ( ͡° ͜ʖ ͡°)   |
+           /|\       |
+           / \       |
+                     |
+         ==============="""
+picWin = """
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠙⠛⠛⠛⠿⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⡀⠠⠤⠒⢂⣉⣉⣉⣑⣒⣒⠒⠒⠒⠒⠒⠒⠒⠀⠀⠐⠒⠚⠻⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⡠⠔⠉⣀⠔⠒⠉⣀⣀⠀⠀⠀⣀⡀⠈⠉⠑⠒⠒⠒⠒⠒⠈⠉⠉⠉⠁⠂⠀⠈⠙⢿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠔⠁⠠⠖⠡⠔⠊⠀⠀⠀⠀⠀⠀⠀⠐⡄⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠉⠲⢄⠀⠀⠀⠈⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠊⠀⢀⣀⣤⣤⣤⣤⣀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠜⠀⠀⠀⠀⣀⡀⠀⠈⠃⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠥⠐⠂⠀⠀⠀⠀⡄⠀⠰⢺⣿⣿⣿⣿⣿⣟⠀⠈⠐⢤⠀⠀⠀⠀⠀⠀⢀⣠⣶⣾⣯⠀⠀⠉⠂⠀⠠⠤⢄⣀⠙⢿⣿⣿
+⣿⡿⠋⠡⠐⠈⣉⠭⠤⠤⢄⡀⠈⠀⠈⠁⠉⠁⡠⠀⠀⠀⠉⠐⠠⠔⠀⠀⠀⠀⠀⠲⣿⠿⠛⠛⠓⠒⠂⠀⠀⠀⠀⠀⠀⠠⡉⢢⠙⣿
+⣿⠀⢀⠁⠀⠊⠀⠀⠀⠀⠀⠈⠁⠒⠂⠀⠒⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢀⣀⡠⠔⠒⠒⠂⠀⠈⠀⡇⣿
+⣿⠀⢸⠀⠀⠀⢀⣀⡠⠋⠓⠤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠈⠢⠤⡀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⡠⠀⡇⣿
+⣿⡀⠘⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠈⠑⡦⢄⣀⠀⠀⠐⠒⠁⢸⠀⠀⠠⠒⠄⠀⠀⠀⠀⠀⢀⠇⠀⣀⡀⠀⠀⢀⢾⡆⠀⠈⡀⠎⣸⣿
+⣿⣿⣄⡈⠢⠀⠀⠀⠀⠘⣶⣄⡀⠀⠀⡇⠀⠀⠈⠉⠒⠢⡤⣀⡀⠀⠀⠀⠀⠀⠐⠦⠤⠒⠁⠀⠀⠀⠀⣀⢴⠁⠀⢷⠀⠀⠀⢰⣿⣿
+⣿⣿⣿⣿⣇⠂⠀⠀⠀⠀⠈⢂⠀⠈⠹⡧⣀⠀⠀⠀⠀⠀⡇⠀⠀⠉⠉⠉⢱⠒⠒⠒⠒⢖⠒⠒⠂⠙⠏⠀⠘⡀⠀⢸⠀⠀⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠑⠄⠰⠀⠀⠁⠐⠲⣤⣴⣄⡀⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⠀⠀⠀⠀⢠⠀⣠⣷⣶⣿⠀⠀⢰⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠁⢀⠀⠀⠀⠀⠀⡙⠋⠙⠓⠲⢤⣤⣷⣤⣤⣤⣤⣾⣦⣤⣤⣶⣿⣿⣿⣿⡟⢹⠀⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠑⠀⢄⠀⡰⠁⠀⠀⠀⠀⠀⠈⠉⠁⠈⠉⠻⠋⠉⠛⢛⠉⠉⢹⠁⢀⢇⠎⠀⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠈⠢⢄⡉⠂⠄⡀⠀⠈⠒⠢⠄⠀⢀⣀⣀⣰⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⢀⣎⠀⠼⠊⠀⠀⠀⠘⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠉⠢⢄⡈⠑⠢⢄⡀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⢀⠀⠀⠀⠀⠀⢻⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⡈⠑⠢⢄⡀⠈⠑⠒⠤⠄⣀⣀⠀⠉⠉⠉⠉⠀⠀⠀⣀⡀⠤⠂⠁⠀⢀⠆⠀⠀⢸⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⡀⠁⠉⠒⠂⠤⠤⣀⣀⣉⡉⠉⠉⠉⠉⢀⣀⣀⡠⠤⠒⠈⠀⠀⠀⠀⣸⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣤⣤⣤⣤⣀⣀⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿
+"""
+
+
+# list of words/phrases to use in game
+wordlist = ("ant", "deer", "badger", "it is what it is.", "python", "hello world!", "dinner", "math :(", "simon", "football", "you lose no matter what!! hahahahahaha", "twinkie", "tavern")
+
+# choose random index from word list
+index = random.randrange(len(wordlist))
+
+# convert random index into a word to use for game
+word = wordlist[index]
+
+
+# starting count and score
+count = 7
+
+# create disguised version of word chosen
+disguise = ""
+
+for i in word:
+    if i != "." and i != "?" and i != "!" and i != "," and i != ":" and i != " " and i != "'" and i != "(" and i != ")":
+        disguise += "-"
+    else:
+        disguise += i
+
+# get length of word chosen
+length = len(word)
+
+# blank list of guessed letters
+guesses = []
+
+# string that will be added to and compared to the answer/word chosen at beginning
+guessWord = disguise
+
+# playing the game
+# ================================================================================================================
+while count > 0:
+    print()
+    print()
+    print()
+# print each picture based on how many guesses are left
+    if count == 7:
+        print(pic1)
+    elif count == 6:
+        print(pic2)
+    elif count == 5:
+        print(pic3)
+    elif count == 4:
+        print(pic4)
+    elif count == 3:
+        print(pic5)
+    elif count == 2:
+        print(pic6)
+    elif count == 1:
+        print(pic7)
+# shows disguised word and number of guesses left
+    print(disguise)
+    print(f"you have {count} guesses left")
+# prints list of guessed letters as long as the list isn't blank
+    if guesses != []:
+        print(f"letters guessed: {guesses}")
+# get input for user guess
+    guess = input("enter a letter to guess: ")
+# add guessed letter to list of guessed letters
+    guesses.append(guess)
+# if letter guessed is a letter inside of the answer word, add the letter to guessWord to compare with the answer and add letter to disguised word
+    for i in range(length):
+        if guess == word[i]:
+            guessWord = guessWord[:i] + guess + guessWord[i + 1:]
+            disguise = disguise[:i] + guess + disguise[i + 1:]
+# check is guessWord matches with answer word and ends game if it does
+    if guessWord == word:
+        count = -1
+# counter decreases
+    count -= 1
+
+print()
+print()
+print()
+print()
+print()
+
+# print results of game
+if count == 0 and guessWord != word:
+    print(pic8)
+    print()
+    print()
+    print(f"sorry you lost, the word/phrase was '{word}'")
+else:
+    print(picWin)
+    print("you win !!!")
